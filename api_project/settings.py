be87d0b699578157de7232ede0e597a96b63e3cf
Jhonny Quiliche <jhonny.jql1210@gmail.com>
2022-12-30 11:09:56 -0500

    UPDATE: BD con mysl

--- api_project/settings.py
+++ api_project/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
+    # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -81,9 +81,17 @@ WSGI_APPLICATION = 'api_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'pagos_project',
+        'USER': 'root',
+        'PASSWORD': '7aah3Dpjhony',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
